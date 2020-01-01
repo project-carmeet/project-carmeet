939fcb177fdde6ec8c03b710c47893875ca8ef23
nusje2000 <maarten.nusteling@gmail.com>
2020-01-01 21:24:36 +0100

    added registration form

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Factory\UserFactory;
+use App\Form\Type\User\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
 use LogicException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -27,6 +31,29 @@ final class SecurityController extends AbstractController
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
+        ]);
+    }
+
+    /**
+     * @Route("/register", name="app_register")
+     */
+    public function register(Request $request, UserFactory $userFactory, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(RegistrationType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $userFactory->createFromUserModel($form->getData());
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'New user has been registered with uuid ' . $user->getId() . '.');
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('security/register.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 
---
+++ src/Form/Type/User/RegistrationType.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form\Type\User;
+
+use App\Model\Form\UserModel;
+use App\Repository\UserRepository;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Callback;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
+
+final class RegistrationType extends AbstractType
+{
+    /**
+     * @var UserRepository
+     */
+    protected $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('username', UsernameType::class, [
+            'label' => 'Username',
+            'constraints' => array_merge(
+                UsernameType::getDefaultConstraints(),
+                [
+                    new NotBlank(),
+                    new Callback([
+                        'callback' => [$this, 'validateUsername'],
+                    ]),
+                ]
+            ),
+        ]);
+
+        $builder->add('email', EmailType::class, [
+            'label' => 'Email',
+            'constraints' => [
+                new NotBlank(),
+                new Callback([
+                    'callback' => [$this, 'validateEmail'],
+                ]),
+            ],
+        ]);
+
+        $builder->add('password', RepeatedType::class, [
+            'type' => PasswordType::class,
+            'first_options' => [
+                'label' => 'Password',
+                'constraints' => [
+                    new NotBlank(),
+                ],
+            ],
+            'second_options' => [
+                'label' => 'Repeat password',
+            ],
+        ]);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => UserModel::class,
+            'required' => false,
+        ]);
+    }
+
+    public function validateUsername(?string $username, ExecutionContextInterface $context): void
+    {
+        if (null !== $username && null !== $this->userRepository->findOneOrNullByUsername($username)) {
+            $context->addViolation('Username is already taken.');
+        }
+    }
+
+    public function validateEmail(?string $email, ExecutionContextInterface $context): void
+    {
+        if (null !== $email && null !== $this->userRepository->findOneOrNullByEmail($email)) {
+            $context->addViolation('User with this email already exists.');
+        }
+    }
+}
