a9210b6a05decf2ffdf62a1d371d8550575201ce
nusje2000 <maarten.nusteling@gmail.com>
2020-01-01 18:10:22 +0100

    added login feature

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_homepage")
+     */
+    public function homepage(): Response
+    {
+        return $this->render('homepage/homepage.html.twig');
+    }
+}
